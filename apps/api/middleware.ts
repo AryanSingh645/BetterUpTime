a49dadf8cc67007b047e6f6e126aaeebd7c7449a
AryanSingh645 <aryansingh645.developer@gmail.com>
2026-04-05 00:47:12 +0530

    middleware added & get website + status routes done

---
+++ apps/api/middleware.ts
@@ -0,0 +1,29 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+
+export const authMiddleware = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    try {
+        const token =
+            req.cookies?.access_token ||
+            req.header("Authorization")?.replace("Bearer ", "");
+        if (!token) {
+            return res.status(400).json({
+                message: "Missing access token",
+                success: false,
+            });
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!); 
+        req.userId = (decoded as { userId: string })?.userId;
+        next();
+    } catch (error) {
+        console.log("Error in middleware:", error);
+        return res.status(403).json({
+            message: "Error in verifying user.",
+            success: false,
+        });
+    }
+};
